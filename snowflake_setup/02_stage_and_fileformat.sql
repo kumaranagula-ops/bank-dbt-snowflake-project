1854fe0181fbe5e06fd52870cd4a5e873be94288
kumaranagula-ops <kumaranagula@gmail.com>
2026-08-24 03:19:12 +0530

    Fixed issues in stage and integration script

--- snowflake_setup/02_stage_and_fileformat.sql
+++ snowflake_setup/02_stage_and_fileformat.sql
@@ -20,20 +20,21 @@ CREATE OR REPLACE FILE FORMAT CSV_STANDARD
 -- OPTION A: Storage Integration (recommended, no hardcoded keys)
 -- Requires an IAM role trust relationship set up in AWS first.
 -- ----------------------------------------------------------------------------
--- CREATE STORAGE INTEGRATION BANK_S3_INT
---   TYPE = EXTERNAL_STAGE
---   STORAGE_PROVIDER = 'S3'
---   ENABLED = TRUE
---   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::<ACCOUNT_ID>:role/<ROLE_NAME>'
---   STORAGE_ALLOWED_LOCATIONS = ('s3://<your-bucket>/bank_savings/');
+CREATE STORAGE INTEGRATION BANK_S3_INT
+   TYPE = EXTERNAL_STAGE
+   STORAGE_PROVIDER = 'S3'
+   ENABLED = TRUE
+   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::139316821088:role/SnowflakeS3AccessRole'
+   STORAGE_ALLOWED_LOCATIONS = ('s3://kumara-sf-bank-proj-139316821088-ap-southeast-2-an/');
 --
--- -- Then run DESC INTEGRATION BANK_S3_INT; and update the AWS role's trust
+-- -- Then run 
+DESC INTEGRATION BANK_S3_INT; --and update the AWS role's trust
 -- -- policy with the STORAGE_AWS_IAM_USER_ARN / STORAGE_AWS_EXTERNAL_ID it returns.
 --
--- CREATE STAGE BANK_S3_STAGE
---   URL = 's3://<your-bucket>/bank_savings/'
---   STORAGE_INTEGRATION = BANK_S3_INT
---   FILE_FORMAT = CSV_STANDARD;
+ CREATE STAGE BANK_S3_STAGE
+ URL = 's3://kumara-sf-bank-proj-139316821088-ap-southeast-2-an/'
+ STORAGE_INTEGRATION = BANK_S3_INT
+ FILE_FORMAT = CSV_STANDARD;
 
 -- ----------------------------------------------------------------------------
 -- OPTION B: Quick-start with an internal stage (no AWS setup needed)
